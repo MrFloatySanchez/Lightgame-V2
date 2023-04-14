e1e66545df4578f01c6242fae2a202703ae270ca
Karlo Sanque <karlo@lasa-berlin.de>
2023-04-14 04:32:10 +0200

    4 Lichtschranken als Array

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,21 @@
 Bei den Pins identische bauteile nebeneinaner anschließen
 */
 
-const int Pin_Lichtschranke = 0;
-const int Pin_LED_Lichtschranke = 7;
-bool status_Lichtschranke;
-const int PIN_RELAYS[4] = { 4, 5, 6, 7 };
+const int Lichtschranken_Anzahl = 4;
+const int Lichtschranke_Pin[Lichtschranken_Anzahl] = { 4, 5, 6, 7 };
+const int Lichtschranke_LED_Pin[Lichtschranken_Anzahl] = { 8, 9, 10, 11 };
+bool Lichtschranke_Status[Lichtschranken_Anzahl];
 
 void
 setup ()
 {
-  pinMode (Pin_Lichtschranke, INPUT);
-  pinMode (Pin_LED_Lichtschranke, OUTPUT);
+  for (int idx = 0; idx < Lichtschranken_Anzahl; idx++)
+    {
+      pinMode (Lichtschranke_Pin[idx], INPUT_PULLUP);
+      pinMode (Lichtschranke_LED_Pin[idx], OUTPUT);
+    }
+  // pinMode (0, INPUT_PULLUP);
+  // pinMode (4, OUTPUT);
   pinMode (LED_BUILTIN, OUTPUT);
   Serial.begin (9600);
 }
@@ -20,16 +25,23 @@ setup ()
 void
 loop ()
 {
-  status_Lichtschranke = digitalRead (Pin_Lichtschranke);
-  Serial.println (status_Lichtschranke);
-  if (status_Lichtschranke)
+  for (int idx = 0; idx < Lichtschranken_Anzahl; idx++)
     {
-      digitalWrite (LED_BUILTIN, HIGH);
-      digitalWrite (Pin_LED_Lichtschranke, LOW);
+      Lichtschranke_Status[idx] = digitalRead (Lichtschranke_Pin[idx]);
+
+      Serial.print (idx);
+      Serial.print (">");
+      Serial.print (Lichtschranke_Status[idx]);
+      Serial.print (" | ");
+
+      if (Lichtschranke_Status[idx])
+        {
+          digitalWrite (Lichtschranke_LED_Pin[idx], HIGH);
+        }
+      else
+        {
+          digitalWrite (Lichtschranke_LED_Pin[idx], LOW);
+        }
     }
-  else
-    {
-      digitalWrite (LED_BUILTIN, LOW);
-      digitalWrite (Pin_LED_Lichtschranke, HIGH);
-    }
+  Serial.println ();
 }
