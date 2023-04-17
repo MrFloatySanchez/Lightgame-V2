c55bb7098ca37bd06274a074064d3c80fddec297
Karlo Sanque <karlo@lasa-berlin.de>
2023-04-17 18:54:38 +0200

    LED Silent_Alarm Logik

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,38 @@
 // define MONITOR auskommentieren um den seriellen Monitor auszulassen
-#define MONITOR
+// #define MONITOR
 
 #include <Arduino.h>
 
+enum Alarm_Status
+{
+  off = 0,
+  on = 1,
+  silent = 2,
+} alarm;
+
 const int pin_Lichtschranke = 8;
 const int pin_LED_Lichtschranke = 4;
-bool lichtschranke_unterbrochen, alarm_on;
+bool lichtschranke_unterbrochen, alarm_on, alarm_last, alarm_silent;
 unsigned long time_current, time_last_flash, time_alarm_last,
-    time_duration_flash = 50, time_duration_alarm = 3000;
+    time_duration_flash = 80, time_duration_flash2 = time_duration_flash * 2,
+    time_duration_alarm = 4000;
+
+void
+alarmTon ()
+{
+}
+
+void
+flash ()
+{
+  if (time_current - time_last_flash > time_duration_flash)
+    {
+      time_last_flash = time_current;
+      static bool temp = LOW;
+      digitalWrite (pin_LED_Lichtschranke, temp);
+      temp = !temp;
+    }
+}
 
 void
 setup ()
@@ -25,64 +50,73 @@ loop ()
 {
   // LVL und Blinkmodi lesen
   // LVL und Blinkmodi schalten
+  // timevergleiche schon durchrechnen?
   time_current = millis ();
   lichtschranke_unterbrochen
       = digitalRead (pin_Lichtschranke); // HIGH > unterbrochen
 
-  // Lichtschranke auswerten
+  // Lichtschranke + Alarmstatus auswerten
   if (lichtschranke_unterbrochen)
     {
-      alarm_on = true;
-      time_alarm_last = time_current;
+      if (alarm == Alarm_Status::off)
+        {
+          time_alarm_last = time_current;
+        }
+      alarm = Alarm_Status::on;
     }
 
-  // Wenn der Alarm schon eine Zeit läuft wieder abschalten
-  if (alarm_on && (time_current - time_alarm_last > time_duration_alarm))
+  // wenn der Alarm schon eine Zeit läuft Ton abschalten, oder ganz ausschalten 
+  if ((time_current - time_alarm_last > time_duration_alarm))
     {
-      alarm_on = false;
-      digitalWrite (pin_LED_Lichtschranke, HIGH);
+      if (alarm == Alarm_Status::on)
+        {
+          alarm = Alarm_Status::silent;
+        }
+      else
+        {
+          alarm = Alarm_Status::off;
+        }
     }
 
-  // Während der alarm läuft die LED blinkne lassen
-  if (alarm_on && (time_current - time_last_flash > time_duration_flash))
+  switch (alarm)
     {
-      time_last_flash = time_current;
-      static bool temp = LOW;
-      digitalWrite (pin_LED_Lichtschranke, temp);
-      temp = !temp;
-    }
-/*
-  if (alarm_on || !(time_current - time_alarm_last > time_duration_alarm))
-    {
-      flash ();
-      ton();
-    }
- */
-// else?
-/*
-  if (time_current - time_alarm_last > time_duration_alarm)
-    {
+    case Alarm_Status::off:
       digitalWrite (pin_LED_Lichtschranke, HIGH);
+      // Pin Alarm abschalten
+      break;
+    case Alarm_Status::on:
+      alarmTon ();
+      if (time_current - time_last_flash > time_duration_flash)
+        {
+          time_last_flash = time_current;
+          static bool temp = LOW;
+          digitalWrite (pin_LED_Lichtschranke, temp);
+          temp = !temp;
+        }
+      break;
+    case Alarm_Status::silent:
+
+      if (time_current - time_last_flash > time_duration_flash2)
+        {
+          time_last_flash = time_current;
+          static bool temp = LOW;
+          digitalWrite (pin_LED_Lichtschranke, temp);
+          temp = !temp;
+        }
+
+      break;
+    default:
+      break;
     }
- */
+
 #ifdef MONITOR
   Serial.print ("> ");
   Serial.print (lichtschranke_unterbrochen);
   Serial.print (" | ");
-  Serial.print (alarm_on);
+  Serial.print (alarm);
+  Serial.print (" | ");
+  Serial.print (time_alarm_last);
   Serial.print (" | ");
   Serial.println ();
 #endif
-}
-
-void
-flash ()
-{
-  if (time_current - time_last_flash > time_duration_flash)
-    {
-      time_last_flash = time_current;
-      static bool temp = LOW;
-      digitalWrite (pin_LED_Lichtschranke, temp);
-      temp = !temp;
-    }
 }
