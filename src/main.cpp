e6ce23ac0e98fd84cd00b4190ace69cc63e3c692
Karlo Sanque <karlo@lasa-berlin.de>
2023-05-02 16:36:47 +0200

    Level-switch angelegt

--- src/main.cpp
+++ src/main.cpp
@@ -52,6 +52,9 @@ bool Alarm_Licht = 0;
 bool Taster_Level_Changed = 0;
 bool Taster_Blink_Changed = 0;
 
+int current_level = 0;
+int current_blink = 0;
+
 unsigned long currenTime;
 unsigned long Alarm_last_time;
 const unsigned long flash_duration = 50;
@@ -62,34 +65,40 @@ struct Modul_Taster
 {
   const int Signal_PIN{}; // INPUT
   const int Led_PIN{};    // OUTPUT
+  const int id{};
 
   bool currentState{};
   bool reading{};
   bool lastButtonState{};
   bool ButtonState{};
+
   unsigned long lastTimePressed{};
 
-  Modul_Taster(int pinSignal, int pinLed)
-    : Signal_PIN(pinSignal)
-    , Led_PIN(pinLed)
+  Modul_Taster(int pinSignal, int pinLed, int nummer)
+      : Signal_PIN(pinSignal), Led_PIN(pinLed), id(nummer)
   {
     currentState = LOW;
   }
 
-  bool checkForTastendruck()
+  bool
+  checkForTastendruck()
   {
     reading = digitalRead(Signal_PIN);
 
-    if (reading != lastButtonState) {
+    if (reading != lastButtonState)
+    {
       lastTimePressed = currenTime;
     }
 
-    if (currenTime - lastTimePressed > debounce_time) {
+    if (currenTime - lastTimePressed > debounce_time)
+    {
 
-      if (reading != ButtonState) {
+      if (reading != ButtonState)
+      {
         ButtonState = reading;
 
-        if (ButtonState == LOW) {
+        if (ButtonState == LOW)
+        {
           currentState = !currentState;
           return true;
           // Taster_Changed = 1;
@@ -102,7 +111,8 @@ struct Modul_Taster
     return false;
   }
 
-  void changeState(bool newState = LOW)
+  void
+  changeState(bool newState = LOW)
   {
     digitalWrite(Led_PIN, newState);
     currentState = newState;
@@ -110,93 +120,106 @@ struct Modul_Taster
 };
 
 Modul_Taster Level_Taster[] = {
-  Modul_Taster(47, 46), Modul_Taster(45, 44), Modul_Taster(49, 48),
-  Modul_Taster(53, 52), Modul_Taster(51, 50),
+    Modul_Taster(47, 46, 0),
+    Modul_Taster(45, 44, 1),
+    Modul_Taster(49, 48, 2),
 };
-// Modul_Taster Blink_Taster[] = {};
-
+/*
+Modul_Taster Blink_Taster[] = {
+    Modul_Taster(43,42,0)
+    Modul_Taster(53, 52, 1),
+    Modul_Taster(51, 50, 2),
+};
+ */
 struct Modul_Lichtschranke
 {
   const int Signal_PIN{};
   const int Led_PIN{};
 
-  bool unterbrochen{ 0 };
-  bool led_state{ 1 };
-  bool flash{ 0 };
+  bool aktiv{1};
+  bool unterbrochen{0};
+  bool led_state{1};
+  bool flash{0};
   unsigned long flash_last_time;
 
   Modul_Lichtschranke(int pinSignal, int pinLed)
-    : Signal_PIN(pinSignal)
-    , Led_PIN(pinLed)
+      : Signal_PIN(pinSignal), Led_PIN(pinLed)
   {
     led_state = HIGH;
     digitalWrite(Led_PIN, led_state);
   }
 
-  void light()
+  void
+  light()
   {
-    if (flash) {
-      if (currenTime - flash_last_time > flash_duration) {
+    if (flash)
+    {
+      if (currenTime - flash_last_time > flash_duration)
+      {
         flash_last_time = currenTime;
         led_state = led_state ? LOW : HIGH;
       }
-    } else {
+    }
+    else
+    {
       led_state = HIGH;
     }
   }
 
-  bool checkForUnterbrechung()
+  bool
+  checkForUnterbrechung()
   {
     return unterbrochen = !digitalRead(Signal_PIN);
   }
 };
 
 Modul_Lichtschranke Alle_Lichtschranken[] = {
-  Modul_Lichtschranke(21, A15),
-  Modul_Lichtschranke(20, A14),
-  Modul_Lichtschranke(19, A13),
-  Modul_Lichtschranke(18, A12),
+    Modul_Lichtschranke(21, A15),
+    Modul_Lichtschranke(20, A14),
+    Modul_Lichtschranke(19, A13),
+    Modul_Lichtschranke(18, A12),
 
 };
 
-void
-alarmSwitch()
+void alarmSwitch()
 {
-  switch (global_Alarm) {
-    case Alarm_Status::off:
-      Alarm_Ton = HIGH;
-      Alarm_Licht = HIGH;
-      break;
-    case Alarm_Status::on:
-      Alarm_Ton = LOW;
-      Alarm_Licht = LOW;
-      break;
-    case Alarm_Status::silent:
-      Alarm_Ton = HIGH;
-      Alarm_Licht = LOW;
-      break;
-    default:
-      break;
+  switch (global_Alarm)
+  {
+  case Alarm_Status::off:
+    Alarm_Ton = HIGH;
+    Alarm_Licht = HIGH;
+    break;
+  case Alarm_Status::on:
+    Alarm_Ton = LOW;
+    Alarm_Licht = LOW;
+    break;
+  case Alarm_Status::silent:
+    Alarm_Ton = HIGH;
+    Alarm_Licht = LOW;
+    break;
+  default:
+    break;
   }
 }
 
-void
-setup()
+void setup()
 {
 #ifdef MONITOR
   Serial.begin(9600);
 #endif
-  for (Modul_Taster& Level : Level_Taster) {
+  for (Modul_Taster &Level : Level_Taster)
+  {
     pinMode(Level.Signal_PIN, INPUT_PULLUP);
     pinMode(Level.Led_PIN, OUTPUT);
   }
-/* 
-  for (Modul_Taster& Blink : Blink_Taster) {
-    pinMode(Blink.Signal_PIN, INPUT_PULLUP);
-    pinMode(Blink.Led_PIN, OUTPUT);
-  }
-   */
-  for (Modul_Lichtschranke& lichtschranke : Alle_Lichtschranken) {
+  /*
+    for (Modul_Taster& Blink : Blink_Taster) {
+      pinMode(Blink.Signal_PIN, INPUT_PULLUP);
+      pinMode(Blink.Led_PIN, OUTPUT);
+    }
+     */
+  for (Modul_Lichtschranke &lichtschranke : Alle_Lichtschranken)
+  {
     pinMode(lichtschranke.Signal_PIN, INPUT_PULLUP);
     pinMode(lichtschranke.Led_PIN, OUTPUT);
   }
@@ -205,19 +228,15 @@ setup()
   pinMode(Alarm_Ton_Pin, OUTPUT);
 }
 
-void
-loop()
+void loop()
 {
   currenTime = millis();
 
-  for (Modul_Taster& Level : Level_Taster) {
-    if (Level.checkForTastendruck()) {
-      Level_Taster[0].currentState = false;
-      Level_Taster[1].currentState = false;
-      Level_Taster[2].currentState = false;
-      Level_Taster[3].currentState = false;
-      Level_Taster[4].currentState = false;
-      Level.changeState(true);
+  for (Modul_Taster &Level : Level_Taster)
+  {
+    if (Level.checkForTastendruck())
+    {
+      current_level = Level.id;
     }
 #ifdef MONITOR
     Serial.print(Level.Signal_PIN);
@@ -226,14 +245,10 @@ loop()
     Serial.print(" | ");
 #endif
   }
-/* 
+/*
   for (Modul_Taster& Blink : Blink_Taster) {
     if (Blink.checkForTastendruck()) {
-      //   Blink_Taster[0].currentState = false;
-      //   Blink_Taster[1].currentState = false;
-      //   Blink_Taster[2].currentState = false;
-      Blink.changeState(HIGH);
-    }
+      current_blink = Blink.id;
 #ifdef MONITOR
     Serial.print(Blink.Signal_PIN);
     Serial.print(" > ");
@@ -245,41 +260,85 @@ loop()
   Serial.println();
 #endif
 
-  for (Modul_Lichtschranke& lichtschranke : Alle_Lichtschranken) {
-    if (lichtschranke.checkForUnterbrechung() == true) {
+  for (Modul_Lichtschranke &lichtschranke : Alle_Lichtschranken)
+  {
+    if (lichtschranke.checkForUnterbrechung() == true)
+    {
       lichtschranke.flash = 1;
-      if (global_Alarm == off) {
+      if (global_Alarm == off)
+      {
         Alarm_last_time = currenTime;
         global_Alarm = on;
       }
     }
   }
 
-  if (global_Alarm == on && (currenTime - Alarm_last_time > AlarmDuration)) {
+  if (global_Alarm == on && (currenTime - Alarm_last_time > AlarmDuration))
+  {
     global_Alarm = silent;
-  } else if (global_Alarm == silent) {
+  }
+  else if (global_Alarm == silent)
+  {
     global_Alarm = off;
-    for (Modul_Lichtschranke& lichtschranke : Alle_Lichtschranken) {
-      if (lichtschranke.unterbrochen) {
+    for (Modul_Lichtschranke &lichtschranke : Alle_Lichtschranken)
+    {
+      if (lichtschranke.unterbrochen)
+      {
         global_Alarm = silent;
         lichtschranke.flash = 1;
-      } else {
+      }
+      else
+      {
         lichtschranke.flash = 0;
       }
     }
   }
 
+  switch (current_level)
+  {
+  case 0:
+    Level_Taster[0].currentState = HIGH;
+    Level_Taster[1].currentState = LOW;
+    Level_Taster[2].currentState = LOW;
+    break;
+  case 1:
+    Level_Taster[0].currentState = LOW;
+    Level_Taster[1].currentState = HIGH;
+    Level_Taster[2].currentState = LOW;
+    break;
+  case 2:
+    Level_Taster[0].currentState = LOW;
+    Level_Taster[1].currentState = LOW;
+    Level_Taster[2].currentState = HIGH;
+    break;
+
+  default:
+    break;
+  }
+
+  switch (current_blink)
+  {
+  case /* constant-expression */:
+    /* code */
+    break;
+
+  default:
+    break;
+  }
+
   alarmSwitch();
 
-  for (Modul_Taster& Level : Level_Taster) {
+  for (Modul_Taster &Level : Level_Taster)
+  {
     digitalWrite(Level.Led_PIN, Level.currentState);
   }
-/* 
-  for (Modul_Taster& Blink : Blink_Taster) {
-    digitalWrite(Blink.Led_PIN, Blink.currentState);
-  }
- */
-  for (Modul_Lichtschranke& lichtschranke : Alle_Lichtschranken) {
+  /*
+    for (Modul_Taster& Blink : Blink_Taster) {
+      digitalWrite(Blink.Led_PIN, Blink.currentState);
+    }
+   */
+  for (Modul_Lichtschranke &lichtschranke : Alle_Lichtschranken)
+  {
     lichtschranke.light();
     digitalWrite(lichtschranke.Led_PIN, lichtschranke.led_state);
   }
